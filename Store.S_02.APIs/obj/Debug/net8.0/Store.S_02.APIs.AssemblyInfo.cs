95c7f5895e18ffa9039b73e14143a7c8cbe1a2e5
marwanabubakr <dev@marobakr>
2024-10-06 15:45:46 +0300

    APi_part_01

--- Store.S_02.APIs/obj/Debug/net8.0/Store.S_02.APIs.AssemblyInfo.cs
+++ Store.S_02.APIs/obj/Debug/net8.0/Store.S_02.APIs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.S_02.APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be7d7ad561fe0fedbb977e3dd4f2e4e4f1cf33e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.S_02.APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.S_02.APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
